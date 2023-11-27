7b1372cb0a6234ebf4b118fdf26d7e2edef65700
Chang Hsin-Han <changhsinhan319@gmail.com>
2023-11-27 20:33:32 +0800

    Initial commit

--- finial.py
+++ finial.py
@@ -13,7 +13,7 @@ import re
 mri_image_dir = '../ADNI_RESIZED2' 
 
 # Use os.listdir -->to get all file names in this directory
-#Don't get the name one by one, which is what I did before
+#Don't get the name one by one (which is what I did before
 mri_image_paths = [os.path.join(mri_image_dir, fname) for fname in os.listdir(mri_image_dir) if fname.endswith('.nii')]
 
 
@@ -63,7 +63,7 @@ def preprocess_and_extract_features_batch(file_paths, model):
 
     # 3D CNN to extract features that we need
     features = model.predict(batch_data)
-    return features.reshape((features.shape[0], -1))  # Reshape
+    return features.reshape((features.shape[0], -1))
 
 # process images one by one
 #features = np.array([preprocess_and_extract_features(path, model) for path in mri_image_paths])
@@ -92,15 +92,15 @@ np.savetxt('../extracted_features.csv', features, delimiter=',')
 #-------------------------------------------------------------------------------------------------------------
 # ... [Your previous code for feature extraction]
 
-# Read the labels CSV file
+# Read
 labels_df = pd.read_csv('../Labels.csv')
 print(labels_df.head())
 
-# Create a dictionary mapping from Image Data ID to label
+# Create a dictionary mapping  Image Data ID and label
 labels_dict = dict(zip(labels_df['Image Data ID'], labels_df['Group']))
 print(list(labels_dict.items())[:5])
 
-# Define a function to extract the Image Data ID from the file name
+# extract the Image Data ID from the file name
 def extract_image_data_id(filename):
     match = re.match(r'.*_(I\d+)\.nii', filename)
     if match:
@@ -111,14 +111,14 @@ def extract_image_data_id(filename):
 mri_image_ids = [extract_image_data_id(os.path.basename(path)) for path in mri_image_paths]
 print(list(mri_image_ids)[:5])
 
-# Debug: Print the first few Image Data IDs and labels
+# Debug
 mri_image_paths = [os.path.join(mri_image_dir, fname) for fname in os.listdir(mri_image_dir) if fname.endswith('.nii')]
 mri_image_labels = [labels_dict.get(extract_image_data_id(os.path.basename(path))) for path in mri_image_paths]
 for path, label in zip(mri_image_paths[:5], mri_image_labels[:5]):
     print(f'File: {path} - Label: {label}')
 
-# Ensure that we only use images for which we have labels
-# Create a list for features and labels where labels are available
+# only use images for which we have labels
+# a list for features and labels where labels are available
 filtered_features = []
 filtered_labels = []
 for image_id, feature in zip(mri_image_ids, features):
@@ -127,21 +127,18 @@ for image_id, feature in zip(mri_image_ids, features):
         filtered_features.append(feature)
         filtered_labels.append(label)
 
-# Convert to numpy arrays
+# Convert 
 filtered_features = np.array(filtered_features)
 filtered_labels = np.array(filtered_labels)
 
 # Debug
 print("First 10 feature-label pairs:")
-for i in range(min(10, len(filtered_features))):  # Ensures we don't go out of bounds
+for i in range(min(10, len(filtered_features))):  
     print(f"Feature {i}:", filtered_features[i])
     print(f"Label {i}:", filtered_labels[i])
 
 
-# Now, filtered_features and filtered_labels have the same length
-# and can be used for training and testing
-
-# Split the dataset into training and testing sets
+# Split the dataset
 X_train, X_test, y_train, y_test = train_test_split(filtered_features, filtered_labels, test_size=0.2, random_state=42)
 #-------------------------------------------------------------------------------------------------------------
 
@@ -156,7 +153,7 @@ y_pred = clf.predict(X_test)
 
 # Save the predictions to a CSV file
 #np.savetxt('predictions.csv', y_pred, delimiter=',')
-np.savetxt('predictions.csv', y_pred, fmt='%s', delimiter=',')
+np.savetxt('../predictions.csv', y_pred, fmt='%s', delimiter=',')
 
 
 # Evaluate the model's performance
@@ -164,10 +161,10 @@ accuracy = accuracy_score(y_test, y_pred)
 report = classification_report(y_test, y_pred)
 
 # Save the classification report to a text file
-with open('classification_report.txt', 'w') as f:
+with open('../classification_report.txt', 'w') as f:
     f.write(report)
 # Save the model's accuracy to a text file
-with open('model_accuracy.txt', 'w') as f:
+with open('../model_accuracy.txt', 'w') as f:
     f.write(f"Accuracy: {accuracy}\n")
 
 # Output the accuracy and classification report
